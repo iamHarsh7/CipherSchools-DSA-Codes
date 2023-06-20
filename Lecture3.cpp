3b9a67e97903ef4f4cb1b0f4f7d2617861bafac5
Harshvardhan S A <KRRISH944@GMAIL.COM>
2023-06-20 18:12:13 +0000

    Lecture 3 codes

---
+++ Lecture3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //declare 2 variables and assign  them values of 3,5 and add them
+
+    int a = 3;
+    int b = 5;
+    //int c;
+    //c = a + b;
+    //cout<<c<<endl;
+    cout<<a+b<<endl;
+
+    cout<<"a+b";
+
+    return 0;
+}
