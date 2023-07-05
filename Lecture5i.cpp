64ff41a192b83600baf7ffa9b35afd087e72ffa8
Harshvardhan S A <KRRISH944@GMAIL.COM>
2023-07-05 19:40:30 +0000

    Lecture 5i codes

---
+++ Lecture5i.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int a,b,c,d,e;
+
+    cout<<"Input 5 integers"<<endl;
+
+    cin>>a>>b>>c>>d>>e;
+
+    cout<<"The integers in reverse order are: "<<e<<d<<c<<b<<a;
+    return 0;
+}
