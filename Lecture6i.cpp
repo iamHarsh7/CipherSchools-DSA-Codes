2f6fbf3493455c3681589c8c538dfa35fc9837d8
Harshvardhan S A <KRRISH944@GMAIL.COM>
2023-07-07 08:05:52 +0000

    Updated

---
+++ Lecture6i.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int i;
+    // for(i=1;i<=100;i++)  // for(initialization;condition;updation){main loop inside flower brackets}
+    // {
+    //     cout<<i<<" ";
+    // }
+
+    // return 0;
+    int i,j,k;
+
+    i=0;
+    j=0;
+    k=0;
+
+    cout<<i<<" "<<j<<" "<<k<<endl;
+    j = i++;
+    cout<<i<<" "<<j<<" "<<k<<endl;
+    k = ++i;
+    cout<<i<<" "<<j<<" "<<k<<endl;
+
+
+
+
+
+}
