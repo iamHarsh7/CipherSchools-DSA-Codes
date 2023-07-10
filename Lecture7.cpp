a9b0b49a2108131bcfbeb1d69222e7d882c355a2
Harshvardhan S A <KRRISH944@GMAIL.COM>
2023-07-10 08:36:03 +0000

    Updated

---
+++ Lecture7.cpp
@@ -0,0 +1,51 @@
+// Password Condition checker
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    for(i=0; ;i++)
+    {
+        cout<<i<<" ";
+        if(i>100) break;
+    }
+    return 0;
+
+    //     int count = 0;
+
+    // while(1) // ! is a boolean value as long as the condition is tru(1) the loop continues infinitely
+    // {
+    //     cout<<"Choclate ";
+    //     count++;
+
+    //     if(count>100)
+    //     {
+    //         break;
+    //     }
+    
+
+    // }
+    //int password;
+    //    do
+    //    {
+    //     cout<<"Enter a password"<<endl;
+    //     cin>>password;
+    
+    //    } while (password<999999); 
+   
+   
+    // int Password;
+
+    // cout<<"Enter a Password"<<endl;
+    // cin>>Password;
+
+    // while(Password<999999)
+    // {
+    //     cout<<"The password didnt meet the requirement re-enter the password"<<endl;
+    //     cin>>Password;
+    // }
+    // cout<<"The password satisfied the requirement"<<endl;
+
+}
