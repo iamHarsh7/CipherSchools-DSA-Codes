3690ba7d0f145703e4b0e8d1005f4d6fe0f333b1
Harshvardhan S A <KRRISH944@GMAIL.COM>
2023-07-17 18:06:24 +0000

    Updated

---
+++ Lecture9i.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+// int sum(int a , int b)
+
+// {
+//     int c;
+//     c = a+b; 
+//     return c;
+// }
+
+// int main()
+// {
+//     int add_sum;
+//     int a,b;
+//     a=3 ; b=5;
+//     add_sum = sum(a,b);
+//     cout<<add_sum;
+
+//     return 0;
+// }
+
+int compare(int a, int b) // Compares 2 integers and returns the Greatest integer
+{
+     a = 5;
+     b = 3;
+    int c;
+    if(a>b)
+    {
+        a=c;
+    }
+    else
+    {
+        c=b;
+    }
+     return c;
+     
+} 
+
+int main()
+{
+    int compare;
+    
+}
